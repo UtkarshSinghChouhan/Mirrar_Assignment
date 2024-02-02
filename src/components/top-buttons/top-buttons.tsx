80228f733afcefdf9d8bf3ece5d53a00709768dc
UtkarshSinghChouhan <uscutkarsh9@gmail.com>
2024-02-02 23:20:50 +0530

    window-object issue fixed

--- src/components/top-buttons/top-buttons.tsx
+++ src/components/top-buttons/top-buttons.tsx
@@ -7,25 +7,10 @@ const TopButtons = ({
 }: {
   setQuery: Dispatch<SetStateAction<string>>;
 }) => {
-  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
-
-  useEffect(() => {
-    const handleResize = () => {
-      setScreenWidth(window.innerWidth);
-    };
-
-    window.addEventListener("resize", handleResize);
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-
-  const itemsToRender = screenWidth <= 768 ? 3 : 5;
   return (
     <>
       <div className="flex items-center justify-around my-6">
-        {CITIES.slice(0, itemsToRender).map((data: ICities) => (
+        {CITIES.map((data: ICities) => (
           <button
             key={data.id}
             className="text-white text-lg font-medium capitalize active:scale-95 hover:text-slate-800"
