2fafece4ececbc0ee3a229b0626d962fa51b58a4
UtkarshSinghChouhan <uscutkarsh9@gmail.com>
2024-02-02 23:31:54 +0530

    responsive top buttons fixed

--- src/components/top-buttons/top-buttons.tsx
+++ src/components/top-buttons/top-buttons.tsx
@@ -7,10 +7,27 @@ const TopButtons = ({
 }: {
   setQuery: Dispatch<SetStateAction<string>>;
 }) => {
+  const [itemsToRender, setItemsToRender] = useState(5);
+
+  useEffect(() => {
+    const handleResize = () => {
+      const width = document.documentElement.clientWidth;
+      const newItemsToRender = width <= 768 ? 3 : 5;
+      setItemsToRender(newItemsToRender);
+    };
+
+    handleResize(); // Call initially
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
   return (
     <>
       <div className="flex items-center justify-around my-6">
-        {CITIES.map((data: ICities) => (
+        {CITIES.slice(0, itemsToRender).map((data: ICities) => (
           <button
             key={data.id}
             className="text-white text-lg font-medium capitalize active:scale-95 hover:text-slate-800"
